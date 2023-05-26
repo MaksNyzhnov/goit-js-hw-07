4f31f5c1ff1f24950db5dce2447e17723da28daa
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-05-26 21:30:06 +0300

    made task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery')
+
+function galleryItemsMarkUp(array) {
+
+    const elements = array.map(item => {
+    
+        const galleryItemEL = document.createElement('li')
+        const gelleryImageEl = document.createElement('img')
+        const galleryLinkEl = document.createElement('a')
+        
+
+
+        galleryItemEL.classList.add('gallery__item')
+
+        galleryLinkEl.classList.add('gallery__link')
+        galleryLinkEl.href = item.preview
+
+        gelleryImageEl.classList.add('gallery__image')
+        gelleryImageEl.dataset.source = item.original
+        gelleryImageEl.src = item.preview
+        gelleryImageEl.alt = item.description
+
+        galleryLinkEl.appendChild(gelleryImageEl)
+        galleryItemEL.appendChild(galleryLinkEl)
+
+        return galleryItemEL
+    });
+    galleryListEl.append(...elements)
+}
+
+galleryItemsMarkUp(galleryItems)
+
+
+
+function onImageClick(event) {
+    event.preventDefault()
+    if (!event.target.nodeName === 'IMG') {
+        return
+    }
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+    instance.show()
+    
+    
+    document.addEventListener('keydown', onEscModalClose)
+    
+    function onEscModalClose(event) {
+    if (!event.code === "Escape") {
+        return
+    }
+        console.log('111')
+    instance.close()
+    document.removeEventListener('keydown', onEscModalClose)
+}
+}
+
+galleryListEl.addEventListener('click', onImageClick)
+
